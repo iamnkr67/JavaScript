d9fde71b73913bddb09cda978fa67e88734b64b2
Nitesh Kumar <niteshkumaro7@hotmail.com>
2023-09-14 11:02:29 +0000

    Objects 02

--- 01Basics/object02.js
+++ 01Basics/object02.js
@@ -1,81 +1,90 @@
-// const tinderUser = new Object()
-const tinderUser = {}
 
-tinderUser.id = "123abc"
-tinderUser.name = "Sammy"
-tinderUser.isLoggedIn = false
+// const tinderUser = new Object() //singelton object
+
+const tinderUser = {}  //non-singelton object
+
+ tinderUser.id = "123abc"
+ tinderUser.name ="Sammy"
+ tinderUser.isLoggedIn = false
+
 
 // console.log(tinderUser);
 
-const regularUser = {
-    email: "some@gmail.com",
-    fullname: {
-        userfullname: {
-            firstname: "nitesh",
-            lastname: "kumar"
+const regularUser ={
+    email : "some@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "nitesh",
+            lastname : "kumar"
         }
     }
 }
 
-// console.log(regularUser.fullname.userfullname.firstname);
+//console.log(regularUser.fullname.userfullname.firstname)
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
-const obj4 = {5: "a", 6: "b"}
+const obj1 = {
+    1 : "a", 
+    2 : "b",
+    3 : "c"
+}
+const obj2 = {
+    4 : "d",
+    5 : "e",
+    6 : "f"
+}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = {
+//     obj1, obj2
+// }
+// const obj3 = Object.assign({}, obj1, obj2)
 
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
-
 const users = [
     {
-        id: 1,
-        email: "nkr@gmail.com"
+        id : 1,
+        email : "nkr@nkr"
     },
+
     {
-        id: 1,
-        email: "nkr@gmail.com"
-    },
-    {
-        id: 1,
-        email: "nkr@gmail.com"
-    },
+        id : 2,
+        email : "nkr@n"
+    }
 ]
 
-users[1].email
-// console.log(tinderUser);
+// console.log(users[1].email)
+// console.log(tinderUser)
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
-
+// console.log(tinderUser.hasOwnProperty('isLogged'))
+// console.log(tinderUser.propertyIsEnumerable('name'))
 
 const course = {
-    coursename: "js",
-    price: "999",
-    courseInstructor: "nitesh"
+    coursename : "js",
+    price : "999",
+    courseInstructor : "nitesh"
 }
 
 // course.courseInstructor
 
-const {courseInstructor: instructor} = course
-
-// console.log(courseInstructor);
-console.log(instructor);
+const {courseInstructor : instructor} = course //destructuring 
+// console.log(courseInstructor)
+console.log(instructor)
 
 // {
-//     "name": "nitesh",
-//     "coursename": "js",
-//     "price": "free"
+//     "name" : "Nitesh",
+//     "coursename" : "Js",
+//     "price" : "free"
 // }
 
-[
-    {},
-    {},
-    {}
-]
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
+

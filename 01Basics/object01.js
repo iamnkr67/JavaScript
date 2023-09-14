e56e3f1dbc42e3b0fc7ae912ca9060c3983ccf14
Nitesh Kumar <niteshkumaro7@hotmail.com>
2023-09-14 10:23:31 +0000

    Objects

--- 01Basics/object01.js
+++ 01Basics/object01.js
@@ -1,38 +1,46 @@
-// singleton
+//objects
+
+//singelton  -> Singleton is a design pattern that tells us that we can create only one instance of a class and that instance can be accessed globally
+
 // Object.create
 
-// object literals
+
+//object literals
 
 const mySym = Symbol("key1")
 
-
 const JsUser = {
-    name: "Nitesh",
-    "full name": "Nitesh Kumar",
+    name : "Nitesh",
+    "full name" : "Nitesh Kumar",
+    age : 18,
     [mySym]: "mykey1",
-    age: 18,
-    location: "Siwan",
-    email: "nitesh@google.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
+    location : "Siwan",
+    email : "nitesh@google.com",      
+    isLoggedIn : false,
+    lastLoginDays : ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+ //console.log(JsUser.email) // got output, but not standard to use JsUser.email
+//  console.log(JsUser["email"]) // standard accessing of objects
+//  console.log(JsUser["full name"])
+//  console.log(JsUser[mySym])
 
-JsUser.email = "nitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "nitesh@microsoft.com"
-// console.log(JsUser);
+//  console.log()
+
+ JsUser.email = "nitesh@nkr.com"
+ //Object.freeze(JsUser)  // no changes can be done now as it freezes JsUser 
+ JsUser.email = "nitesh@microsoft.com"
+
+//  console.log(JsUser);
 
 JsUser.greeting = function(){
-    console.log("Hello JS user");
-}
-JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
-}
+    console.log("Hello Js User")
+ }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+ JsUser.greetingTwo = function(){
+    console.log(`Hello Js User, ${this.name}`)
+ }
+
+ JsUser.greeting()
+ JsUser.greetingTwo()
+

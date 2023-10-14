a70b35e3ff8572b40b8d0547b065c03cb435a8d5
Nitesh Kumar <niteshkumaro7@hotmail.com>
2023-10-14 17:18:22 +0000

    BASIC BUILT-IN METHODS

---
+++ 01Basics/basic_builtinMethods.js
@@ -0,0 +1,37 @@
+/*
+BASIC BUILT IN METHODS
+----------------------
+
+ Math.round()
+ Math.random()
+ Math.floor()
+ Math.sqrt()
+ Math.sin()
+ Math.cos()
+
+ String Functions
+ ---------------
+ string.length
+ string.indexOf()
+ string.toUpperCase()
+ string.toLowerCase()
+ string.replace()
+
+ Array Functions
+ ---------------
+ array.length
+ array.sort()
+ array.filter()
+ array.map()
+ array.reduce()
+
+ Date Functions
+ --------------
+ Date.now()
+ date.toLocalDateString()
+ date.toISOString()
+ date.getTime()
+ date.getFullYear()
+
+ 
+*/
